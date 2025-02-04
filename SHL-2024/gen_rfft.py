44bead41536d0f21d1d179adb95de1216e44e06e
ben0i0d <ben0i0d@foxmail.com>
2025-02-04 17:09:04 +0800

    add jax

--- SHL-2024/gen_rfft.py
+++ SHL-2024/gen_rfft.py
@@ -3,41 +3,56 @@ import psutil
 import numpy as np
 from tqdm import tqdm
 from shutil import copyfile
-from joblib import Parallel , delayed
 from numpy.lib.format import open_memmap
 
-def window_rfft(tmp, window_length=100, step=20, fs=100):
+import jax
+import jax.numpy as jnp
+
+from functools import partial
+
+@partial(jax.jit,static_argnames=('window_length', 'step', 'fs'))
+def window_rfft(signal, window_length=100, step=20, fs=100):
     """
     Computes the RFFT for segments of signals using a sliding window.
     """
-    data = [[] for _ in tmp]
-    for i,signal in enumerate(tmp):
-        signal_data = []
-        for start in range(0, len(signal) - window_length + 1, step):
-            window = signal[start:start + window_length]
-            window = window - np.mean(window)
-            rfft_result = np.abs(np.fft.rfft(window))
-            rfft_result /= (fs / 2)
-            rfft_result[0] /= 2
-            signal_data.append(rfft_result)
-        data[i] = np.array(signal_data).reshape(-1)
-
-    return data
+    n_frames, frame_length = signal.shape
+    n_subframes = 1 + (frame_length - window_length) // step
+    
+    starts = jnp.arange(n_subframes) * step
+    def get_window(frame, start):
+        return jax.lax.dynamic_slice(frame, (start,), (window_length,))
+    signal = jax.vmap(
+        jax.vmap(get_window, in_axes=(None, 0)),  
+        in_axes=(0, None)                         
+    )(signal, starts)
+    
+    signal = signal - jnp.mean(signal, axis=2, keepdims=True)
+    signal = jnp.abs(jnp.fft.rfft(signal))
+    signal /= (fs / 2)
+    signal = signal.at[...,0].set(signal[..., 0]/2)
+    return signal.reshape(n_frames, -1)
 
 def gen_rfft(dataset, shape):
-    source = np.load('data/npy_data/{}/data.npy'.format(dataset),mmap_mode='r')
+    source = jnp.load('data/npy_data/{}/data.npy'.format(dataset),mmap_mode='r')
     data = open_memmap('data/fft_data/{}/data.npy'.format(dataset),dtype=np.float16,mode='w+',shape=shape)
-    for i in tqdm(range(shape[0])):
-        Parallel(n_jobs=psutil.cpu_count(logical=False), verbose=0)(delayed(lambda j: data[i].__setitem__(j,window_rfft(source[i,j])))(j) for j in range(shape[1]))
-
+    for i in range(shape[0]):
+        
+        for j in tqdm(range(shape[1])):
+            data[i].__setitem__(j,window_rfft(source[i,j]))
+        
+        #data[i] = jax.vmap(window_rfft_jnp)(source[i])
+            
 def gen_test(shape=(9, 92726, 1071)):
     if not os.path.exists('data/fft_data/test'):
         os.mkdir('data/fft_data/test')
-    #生成数据
-    source = np.load('data/npy_data/test/data.npy',mmap_mode='r')
+    
+    source = jnp.load('data/npy_data/test/data.npy',mmap_mode='r')
     data = open_memmap('data/fft_data/test/data.npy',dtype=np.float16,mode='w+',shape=shape)
-
-    Parallel(n_jobs=psutil.cpu_count(logical=False), verbose=0)(delayed(lambda i: data.__setitem__(i,window_rfft(source[i])))(i) for i in tqdm(range(shape[0])))
+    
+    for i in tqdm(range(shape[0])):
+        data.__setitem__(i,window_rfft(source[i]))
+    
+    #data = jax.vmap(window_rfft_jnp)(source)
 
 if __name__ == '__main__':
 
@@ -57,14 +72,14 @@ if __name__ == '__main__':
     
     # copy label
     for set in sets:
-        print("processing {} Label".format(set))
         copyfile('data/npy_data/{}/label.npy'.format(set),'data/fft_data/{}/label.npy'.format(set))
+        print("processed {} Label".format(set))
 
     # gen test
-    print("processing test data")
     gen_test()
+    print("processed test data")
 
     # gen_rfft
     for i,set in enumerate(sets):
-        print("processing {}".format(set))
         gen_rfft(set, shape_list[i])
+        print("processed {}".format(set))
