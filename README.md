6800f795823fab92f7f601fc607239b0fb862918
ben0i0d <ben0i0d@foxmail.com>
2025-02-07 03:43:42 +0800

    use lax.map replace vmap

--- README.md
+++ README.md
@@ -19,13 +19,12 @@ Data preparation code to provide consistent and high-performance processing
 
 ## Target needs
 
-1. Mem：Try to minimize memory consumption
+1. Mem：Up to 16GB
 2. Time：Faster as well as possible
 
 ## Be care
 
 1. Data preprocessing uses a lot of performance optimizations, the goal of which is trying to strike a reasonable balance between speed and demand
-2. If you are willing to bear the memory consumption, you can replace 'open_memmap' with 'np.load' or 'np.save' to speed up the processing
-3. This project has a high demand for I/O to ensure that it works on a medium with high I/O capability
-4. Some datasets have different preprocessing patterns in different projects, and to avoid ambiguity, I have indicated the source in "[]".
-5. `N C T V M` is `Num Channel Frames Joint Body`
+2. This project has a high demand for I/O to ensure that it works on a medium with high I/O capability
+3. Some datasets have different preprocessing patterns in different projects, and to avoid ambiguity, I have indicated the source in "[]".
+4. `N C T V M` is `Num Channel Frames Joint Body`
--- SHL-2024/gen_rfft.py
+++ SHL-2024/gen_rfft.py
@@ -10,49 +10,39 @@ import jax.numpy as jnp
 
 from functools import partial
 
-@partial(jax.jit,static_argnames=('window_length', 'step', 'fs'))
+@partial(jax.jit, static_argnames=('window_length', 'step', 'fs'))
 def window_rfft(signal, window_length=100, step=20, fs=100):
     """
-    Computes the RFFT for segments of signals using a sliding window.
+    Computes the RFFT for segments of 1D signal using a sliding window.
     """
-    n_frames, frame_length = signal.shape
-    n_subframes = 1 + (frame_length - window_length) // step
+    n_subframes = 1 + (signal.shape[0] - window_length) // step
     
     starts = jnp.arange(n_subframes) * step
-    def get_window(frame, start):
-        return jax.lax.dynamic_slice(frame, (start,), (window_length,))
-    signal = jax.vmap(
-        jax.vmap(get_window, in_axes=(None, 0)),  
-        in_axes=(0, None)                         
-    )(signal, starts)
-    
-    signal = signal - jnp.mean(signal, axis=2, keepdims=True)
+    signal = jax.vmap(lambda start: jax.lax.dynamic_slice(signal, (start,), (window_length,)))(starts)
+    # 去均值
+    signal = signal - jnp.mean(signal, axis=1, keepdims=True)
+    # 计算RFFT
     signal = jnp.abs(jnp.fft.rfft(signal))
-    signal /= (fs / 2)
-    signal = signal.at[...,0].set(signal[..., 0]/2)
-    return signal.reshape(n_frames, -1)
+    signal /= (fs / 2)  # 归一化
+    # 调整直流分量
+    signal = signal.at[:, 0].set(signal[:, 0] / 2)
+    return signal.reshape(-1)
 
-def gen_rfft(dataset, shape):
-    source = jnp.load('data/npy_data/{}/data.npy'.format(dataset),mmap_mode='r')
+def gen_rfft(dataset, shape, batchsize):
+    source = np.load('data/npy_data/{}/data.npy'.format(dataset),mmap_mode='r')
     data = open_memmap('data/fft_data/{}/data.npy'.format(dataset),dtype=np.float16,mode='w+',shape=shape)
-    for i in range(shape[0]):
-        
-        for j in tqdm(range(shape[1])):
-            data[i].__setitem__(j,window_rfft(source[i,j]))
-        
-        #data[i] = jax.vmap(window_rfft_jnp)(source[i])
+    for i in tqdm(range(shape[0])):
+        data[i] = jax.lax.map(window_rfft,source[i].reshape(-1,500),batch_size=batchsize).reshape(shape[1],shape[2],shape[3])
             
-def gen_test(shape=(9, 92726, 1071)):
+def gen_test(batchsize, shape=(9, 92726, 1071)):
     if not os.path.exists('data/fft_data/test'):
         os.mkdir('data/fft_data/test')
     
-    source = jnp.load('data/npy_data/test/data.npy',mmap_mode='r')
+    source = np.load('data/npy_data/test/data.npy',mmap_mode='r')
     data = open_memmap('data/fft_data/test/data.npy',dtype=np.float16,mode='w+',shape=shape)
     
     for i in tqdm(range(shape[0])):
-        data.__setitem__(i,window_rfft(source[i]))
-    
-    #data = jax.vmap(window_rfft_jnp)(source)
+        data[i] = jax.lax.map(window_rfft,source[i].reshape(-1,500),batch_size=batchsize).reshape(shape[1],shape[2])
 
 if __name__ == '__main__':
 
@@ -63,6 +53,8 @@ if __name__ == '__main__':
 
     modal_list = ("Bag","Hand","Hips","Torso")
 
+    batchsize = 65536
+    
     if not os.path.exists('data/fft_data'):
         os.mkdir('data/fft_data')
         for set in sets:
@@ -76,10 +68,10 @@ if __name__ == '__main__':
         print("processed {} Label".format(set))
 
     # gen test
-    gen_test()
+    gen_test(batchsize)
     print("processed test data")
 
     # gen_rfft
     for i,set in enumerate(sets):
-        gen_rfft(set, shape_list[i])
+        gen_rfft(set, shape_list[i], batchsize)
         print("processed {}".format(set))
